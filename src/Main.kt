856fc1ed2ed177213abd6f5ae31060df690886de
Phillip Schmitte <phil@fieldlens.com>
2021-11-09 20:45:48 -0800

    Modified map structure

--- src/Main.kt
+++ src/Main.kt
@@ -1,12 +1,16 @@
 import calculations.PathComplexity
 import generators.Systematic
 import model.createConstellations
+import print.printConstellationData
 import print.printMythData
 import print.writeToFile
 
 fun main(){
+    val constellationOutput = createConstellations()
+    printConstellationData(constellationOutput.allConstellations)
+
     val pointStrategy = PathComplexity()
-    val myths = Systematic().generateMyths(createConstellations(), pointStrategy)
+    val myths = Systematic().generateMyths(constellationOutput, pointStrategy)
     printMythData(myths, pointStrategy)
     writeToFile(myths, pointStrategy)
 }
